fbd333d8885dc267edca4b0203cf9c9d4350a54d
JessTells <jernestot27@gmail.com>
2023-08-10 18:01:02 -0700

    did a couple exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, amt) {
+    if(amt < 0){
+        return "ERROR"
+    }
+    let s = "";
+    for(i = 0; i < amt; ++i){
+        s += str;
+    };
+    return s; 
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const newArr = [];
 
+    arr.forEach(element => {
+        if (!args.includes(element)) {
+            newArr.push(element);
+        }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
